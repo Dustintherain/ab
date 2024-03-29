8b5df2b32ae982a6ec2384ea173d349b657c7d5d
Dustintherain <164018870+Dustintherain@users.noreply.github.com>
2024-03-29 20:21:27 +0800

    Add files via upload

---
+++ src/storage/framework/views/fe1defc3aa396f8c89e809fa6b17c97d6b60e34f.php
@@ -0,0 +1,7 @@
+<center>
+    <h1>会员激活认证</h1>
+    <p style="font-size: 12px;color: grey">
+        <?php echo e($username); ?>，您好！感谢你注册<?php echo e($webName); ?>，请点击下面链接完成激活认证你的账户。<br>
+        <a href="<?php echo e($url); ?>"><?php echo e($url); ?></a>
+    </p>
+</center><?php /**PATH /home/ftp/s/ser177390476772/ym/src/resources/views/email/verify.blade.php ENDPATH**/ ?>
